521a868c92a138f335f2f0e38f1872dd540202db
Colin Wilson <colin.wilson@protonmail.ch>
2017-06-11 04:05:54 +0100

    add command-line argument options

--- kemp-cert-update.sh
+++ kemp-cert-update.sh
@@ -2,14 +2,36 @@
 
 VER=1.0.0
 
-#Define variables
+while [ -n "$1" ]
+ 
+do
+ 
+    case "$1" in
 
-KEMP_IP="172.16.2.10"
-KEMP_USER_CERT_PATH="/tmp/certs/cert-auto-update.cert.pem"
-CERT_NAME="fw02-pfsense-sbg-1.qunux.com"
+        -f|--file)
+            KEMP_USER_CERT_PATH="$2"
+            shift # past argument
+            ;;
+            -d|--domain)
+            CERT_NAME="$2"
+            shift # past argument
+            ;;
+            -i|--ipaddress)
+            KEMP_IP="$2"
+            shift # past argument
+            ;;
+            --default)
+            DEFAULT=YES
+            ;;
+            *)
+                    # unknown option
+            ;;
+    esac
+
+    shift # past argument or value
+done
 
 LIST_CERTS=$(curl -sS -k -E $KEMP_USER_CERT_PATH https://${KEMP_IP}/access/listcert | xmllint --format --xpath "boolean(//name[text()='$CERT_NAME'])" - )
-#echo "$LIST_CERTS"
 
 cd /tmp/acme/${CERT_NAME}/${CERT_NAME}/ && cat $CERT_NAME.cer $CERT_NAME.key >> $CERT_NAME.full.pem && cd /tmp/acme/${CERT_NAME}/${CERT_NAME}/
 
