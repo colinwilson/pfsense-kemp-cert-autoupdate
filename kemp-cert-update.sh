3c497bf8a3a5f4c8dd22271542bc859d83bdd00b
Colin Wilson <colin.wilson@protonmail.ch>
2017-06-12 21:52:19 +0100

    update

--- kemp-cert-update.sh
+++ kemp-cert-update.sh
@@ -29,19 +29,23 @@ do
     esac
 
     shift # past argument or value
+    
 done
 
 LIST_CERTS=$(curl -sS -k -E $KEMP_USER_CERT_PATH https://${KEMP_IP}/access/listcert | xmllint --format --xpath "boolean(//name[text()='$CERT_NAME'])" - )
 
-cd /tmp/acme/${CERT_NAME}/${CERT_NAME}/ && cat $CERT_NAME.cer $CERT_NAME.key >> $CERT_NAME.full.pem && cd /tmp/acme/${CERT_NAME}/${CERT_NAME}/
+cat /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.cer /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.key > /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.full.pem
 
 if [ "$LIST_CERTS" = true ] ; then
-REPLACE=1
+    REPLACE=1
 else
-REPLACE=0
+    REPLACE=0
 fi
 
-UPLOAD=$(curl -sS -X POST --data-binary "@${CERT_NAME}.full.pem" -k -E $KEMP_USER_CERT_PATH "https://${KEMP_IP}/access/addcert?cert=${CERT_NAME}&replace=${REPLACE}")
-echo "$UPLOAD"
+upload_cert() {
+    curl -sS -X POST --data-binary "@/tmp/acme/${CERT_NAME}/${CERT_NAME}/${CERT_NAME}.full.pem" -k -E $KEMP_USER_CERT_PATH "https://${KEMP_IP}/access/addcert?cert=${CERT_NAME}&replace=${REPLACE}"
+}
 
-rm /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.full.pem && cd ~
+upload_cert
+
+rm /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.full.pem
