09b632d28f7f2bacd12d8c8cb5315c9e07b942bc
Colin Wilson <colin.wilson@protonmail.ch>
2017-06-12 22:56:00 +0100

    add comments

--- kemp-cert-update.sh
+++ kemp-cert-update.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-VER=1.0.0
+###################################################################################
+# Title: Auto-Update/Upload LetsEncrypt Certs to KEMP LoadMaster
+# Date: 12/06/2017
+# Author: Colin Wilson (colin@qunux.com) / Qunux Consulting Ltd
+# Vendor or Software Link: https://www.pfsense.org/ / https://kemptechnologies.com
+# Version: 1.0.0
+# Category: Shell Script
+# Tested on: pfSense 2.3.4 & KEMP LM 7.2.38
+###################################################################################
 
 while [ -n "$1" ]
  
@@ -32,9 +40,8 @@ do
     
 done
 
+# Check if certificate name exists on KEMP LoadMaster
 LIST_CERTS=$(curl -sS -k -E $KEMP_USER_CERT_PATH https://${KEMP_IP}/access/listcert | xmllint --format --xpath "boolean(//name[text()='$CERT_NAME'])" - )
-
-cat /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.cer /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.key > /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.full.pem
 
 if [ "$LIST_CERTS" = true ] ; then
     REPLACE=1
@@ -42,10 +49,15 @@ else
     REPLACE=0
 fi
 
+# Concatenate certificate and key
+cat /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.cer /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.key > /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.full.pem
+
+# Upload certificate to KEMP LoadMaster
 upload_cert() {
     curl -sS -X POST --data-binary "@/tmp/acme/${CERT_NAME}/${CERT_NAME}/${CERT_NAME}.full.pem" -k -E $KEMP_USER_CERT_PATH "https://${KEMP_IP}/access/addcert?cert=${CERT_NAME}&replace=${REPLACE}"
 }
 
 upload_cert
 
+# Delete concatenated certificate file
 rm /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.full.pem
