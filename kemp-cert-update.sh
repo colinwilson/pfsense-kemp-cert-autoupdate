f804b3ddc3605ee7d9d453e0b3682ecf31efa56f
Colin Wilson <colin.wilson@protonmail.ch>
2017-06-22 03:36:29 +0100

    Add option for basic authentication
    Add simple check for authentication method

--- kemp-cert-update.sh
+++ kemp-cert-update.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-###################################################################################
+#
 # Title: Auto-Update/Upload LetsEncrypt Certs to KEMP LoadMaster
 # Date: 12/06/2017
-# Author: Colin Wilson (colin@qunux.com) / Qunux Consulting Ltd
-# Vendor or Software Link: https://www.pfsense.org/ / https://kemptechnologies.com
+# Author: Colin Wilson @colinwilson / Qunux Consulting Ltd
+# Vendor or Software Link: https://www.pfsense.org/ , https://kemptechnologies.com
 # Version: 1.0.0
 # Category: Shell Script
 # Tested on: pfSense 2.3.4 & KEMP LM 7.2.38
-###################################################################################
+#
 
 while [ -n "$1" ]
  
@@ -18,6 +18,10 @@ do
 
         -f|--file)
             KEMP_USER_CERT_PATH="$2"
+            shift # past argument
+            ;;
+            -b|--basicauth)
+            BASIC_AUTH="$2"
             shift # past argument
             ;;
             -d|--domain)
@@ -41,7 +45,21 @@ do
 done
 
 # Check if certificate name exists on KEMP LoadMaster
+if [ -z "$KEMP_USER_CERT_PATH" ]
+then
+    :
+else
 LIST_CERTS=$(curl -sS -k -E $KEMP_USER_CERT_PATH https://${KEMP_IP}/access/listcert | xmllint --format --xpath "boolean(//name[text()='$CERT_NAME'])" - )
+
+fi
+
+if [ -z "$BASIC_AUTH" ]
+then
+    :
+else
+LIST_CERTS=$(curl -sS -k https://${BASIC_AUTH}@${KEMP_IP}/access/listcert | xmllint --format --xpath "boolean(//name[text()='$CERT_NAME'])" - )
+
+fi
 
 if [ "$LIST_CERTS" = true ] ; then
     REPLACE=1
@@ -53,11 +71,26 @@ fi
 cat /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.cer /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.key > /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.full.pem
 
 # Upload certificate to KEMP LoadMaster
-upload_cert() {
-    curl -sS -X POST --data-binary "@/tmp/acme/${CERT_NAME}/${CERT_NAME}/${CERT_NAME}.full.pem" -k -E $KEMP_USER_CERT_PATH "https://${KEMP_IP}/access/addcert?cert=${CERT_NAME}&replace=${REPLACE}"
-}
+if [ -z "$BASIC_AUTH" ]
+then
+    :
+else
+      upload_cert_basic() {
+            curl -sS -X POST --data-binary "@/tmp/acme/${CERT_NAME}/${CERT_NAME}/${CERT_NAME}.full.pem" -k "https://${BASIC_AUTH}@${KEMP_IP}/access/addcert?cert=${CERT_NAME}&replace=${REPLACE}"
+        }
 
-upload_cert
+      upload_cert_basic
+fi
+
+if [ -z "$KEMP_USER_CERT_PATH" ]
+then
+    :
+else
+      upload_cert() {
+            curl -sS -X POST --data-binary "@/tmp/acme/${CERT_NAME}/${CERT_NAME}/${CERT_NAME}.full.pem" -k -E $KEMP_USER_CERT_PATH "https://${KEMP_IP}/access/addcert?cert=${CERT_NAME}&replace=${REPLACE}"
+        }
+      upload_cert
+fi
 
 # Delete concatenated certificate file
 rm /tmp/acme/${CERT_NAME}/${CERT_NAME}/$CERT_NAME.full.pem
